d1c0f47eadb67a614cb3f1a7c87b6a125ebe2a70
Gavin <gavin.monkeystudios@gmail.com>
2024-12-06 18:01:16 -0700

    Space saving

--- src/waves.lua
+++ src/waves.lua
@@ -1,5 +1,5 @@
 -- Constants
-WAVE_PREP_TIME = 15 * 30 -- 15 seconds (PICO-8 runs at 30 FPS)
+WAVE_PREP_TIME = 450 -- 15 seconds (PICO-8 runs at 30 FPS)
 
 -- Variables
 wave_number = 1
@@ -8,9 +8,6 @@ wave_units_to_spawn = 0
 wave_spawning_unit_type = nil
 wave_is_elite = false
 wave_running = false
-
--- Wave variables
-boss_waves = {10, 30}
 elite_waves = {7, 13, 19, 23, 27}
 
 -- Update wave timing and spawning units
@@ -28,7 +25,6 @@ function update_waves()
         game_state = 'victory'
     end
 
-    -- Countdown timer if the wave is no longer running
     if not wave_running and wave_timer > 0 then
         wave_timer -= 1
     end
@@ -37,26 +33,18 @@ function update_waves()
         start_wave()
         wave_number += 1
         if wave_number <= 30 then
-            wave_timer = WAVE_PREP_TIME -- 20 seconds after all units destroyed
+            wave_timer = WAVE_PREP_TIME
         end
     end
 end
 
--- Prepare the wave by prespawning units
 function prep_wave()
-    
     wave_is_elite = (contains(elite_waves, wave_number))
     wave_spawning_unit_type = next_unit_type
     wave_units_to_spawn = lookup(wave_spawning_unit_type, 'spawn_number', get_wave_unit_total(wave_number))
     wave_units_to_spawn += flr(wave_number / 20) * game_difficulty
-
-    -- Bosses have their own fixed number of spawns
-    -- if wave_spawning_unit_type.type == 'boss' then
-    --     wave_units_to_spawn = wave_spawning_unit_type.spawn_number
-    -- end
 end
 
--- Start a new wave of units
 function start_wave()
     next_unit_type = get_next_unit_type()
     wave_running = true
@@ -66,11 +54,9 @@ function get_wave_unit_total(wave)
     return 2 + ceil(wave / 3)
 end
 
--- Determine the next unit type based on probabilities
 function get_next_unit_type()
     local next_wave = wave_number + 1
 
-    -- Define basic unit probabilities
     local unit_probs = {
         {type = unit_types_list['Walker'], prob = 0.30},
         {type = unit_types_list['Knight'], prob = 0.25},
@@ -78,8 +64,6 @@ function get_next_unit_type()
         {type = unit_types_list['Bat'], prob = 0.20}
     }
 
-    -- Override with boss probabilities for boss waves
-    -- 10, 15, 25, 30
     if next_wave == 10 or next_wave == 25 then
         unit_probs = {{type = unit_types_list['Carrier'], prob = 1.0},}
     elseif next_wave == 15 or next_wave == 30 then
@@ -92,7 +76,6 @@ function get_next_unit_type()
         }
     end
 
-    -- Randomly select unit type based on probabilities
     local r = rnd(1)
     local cumulative = 0
     for i = 1, #unit_probs do
